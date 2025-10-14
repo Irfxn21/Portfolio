4db5b5d5ecafcd45f6d004f4c3f4cb67bbf7f3f7
Irfxn21 <shahid.irfan1107@gmail.com>
2025-10-13 21:22:03 -0400

    Update projects section

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -4,30 +4,60 @@ import { ArrowRight, ExternalLink, Github } from "lucide-react";
 const projects = [
     {
         id: 1,
-        title: "Saas Landing Page",
-        description: "Beautiful landing page app using react and tailwind",
-        image: "/projects/sample.png",
-        tags: ["React", "TailwindCSS", "Supabase"],
+        title: "NutriLens",
+        description: "Full-stack web application that analyzes uploaded food images to estimate calorie and macronutrient values inreal time",
+        image: "/projects/nutrilens.png",
+        tags: ["React", "TailwindCSS", "Flask", "TensorFlow"],
         demoUrl: "#",
         githubUrl: "#",
     },
 
     {
         id: 2,
-        title: "Saas Landing Page",
-        description: "Beautiful landing page app using react and tailwind",
-        image: "/projects/sample.png",
-        tags: ["React", "TailwindCSS", "Supabase"],
+        title: "Minesweeper",
+        description: "Star Wars inspired version of Minesweeper on linux, playable on both command line and graphical interface",
+        image: "/projects/minesweeper.png",
+        tags: ["C", "GTK", "Linux"],
         demoUrl: "#",
         githubUrl: "#",
     },
 
     {
         id: 3,
-        title: "Saas Landing Page",
-        description: "Beautiful landing page app using react and tailwind",
-        image: "/projects/sample.png",
-        tags: ["React", "TailwindCSS", "Supabase"],
+        title: "Premier League Match Predictor",
+        description: "Machine learning model used to predict Premier League soccer matches",
+        image: "/projects/soccer.png",
+        tags: ["Python", "Pandas","BS4", "Scikit-Learn"],
+        demoUrl: "#",
+        githubUrl: "#",
+    },
+
+    {
+        id: 4,
+        title: "Amazon Website Clone",
+        description: "Recreation of a sample Amazon Website with user interactivity, including features like cart management and checkout",
+        image: "/projects/amazon.png",
+        tags: ["Python", "Pandas","BS4", "Scikit-Learn"],
+        demoUrl: "#",
+        githubUrl: "#",
+    },
+
+    {
+        id: 5,
+        title: "Maze Runner",
+        description: "Maze solving algorithm that computes an escape path for any given maze",
+        image: "/projects/maze.png",
+        tags: ["Java", "Maven"],
+        demoUrl: "#",
+        githubUrl: "#",
+    },
+
+    {
+        id: 6,
+        title: "Portfolio",
+        description: "Machine learning model used to predict Premier League soccer matches",
+        image: "/projects/portfolio.png",
+        tags: ["React", "Tailwind CSS","Vite"],
         demoUrl: "#",
         githubUrl: "#",
     },
@@ -36,18 +66,17 @@ const projects = [
 
 export const ProjectsSection = () => {
 
-
     return <section id="projects" className="py-24 px-4 relative">
-        <div className="container mx-auto max-w-5xl">
-            <h2 className="text-3xl md:text-4xl font-bold mb-4 text-center">Featured <span className="text-primary"> Projects</span></h2>
+        <div className="container mx-auto max-w-6xl">
+            <h2 className="text-3xl md:text-5xl font-bold mb-4 text-center">Featured <span className="text-primary"> Projects</span></h2>
 
-            <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto">
+            <p className="text-center text-muted-foreground mb-12 max-w-2xl mx-auto text-lg">
                 Here are some of my recent projects.
             </p>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 ">
                 {projects.map((project, key) => (
-                    <div key={key} className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover">
+                    <div key={key} className="group bg-card rounded-lg overflow-hidden shadow-xs card-hover flex flex-col h-full">
                         <div className="h-48 overflow-hidden">
                             <img 
                                 src={project.image} 
@@ -56,10 +85,10 @@ export const ProjectsSection = () => {
                             />
                         </div>
 
-                        <div className="p-6">
+                        <div className="p-6 flex flex-col flex-grow">
                             <div className="flex flex-wrap gap-2 mb-4">
                                 {project.tags.map((tag) => (
-                                    <span className="px-2 py-1 text-xs font-mewdium border rounded-full bg-secondary text-secondary-foreground">
+                                    <span className="px-2 py-1 text-xs font-medium border rounded-full bg-secondary text-secondary-foreground">
                                         {tag}
                                     </span>
                                 ))}
@@ -67,21 +96,16 @@ export const ProjectsSection = () => {
                         
                             <h3 className="text-xl font-semibold mb-1">{project.title}</h3>
                             <p className="text-muted-foreground text-sm mb-4">{project.description}</p>
-                            <div className="flex justify-between items-center">
+                            <div className="flex justify-start mt-auto">
                                 <div className="flex space-x-3">
-                                    <a 
-                                        href={project.demoUrl} 
-                                        target="_blank"
-                                        className="text-foreground/80 hover:text-primary transition-colors duration-300">
-                                        <ExternalLink size={20} />
-                                    </a>
-
                                     <a 
                                         href={project.githubUrl}
                                         target="_blank"
-                                        className="text-foreground/80 hover:text-primary transition-colors duration-300">
-                                        <Github size={20} />
+                                        className="flex items-center gap-1 text-foreground/80 hover:text-primary transition-colors duration-300">
+                                        <Github size={21}/>
+                                        <p>GitHub</p>
                                     </a>
+
                                 </div>
                             </div>
                         </div>
